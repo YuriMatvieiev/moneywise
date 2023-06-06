46105c199c2efd62c369197d87dac51dda626d3f
Yuri Matvieiev <matvssv.yura@gmail.com>
2023-06-06 16:12:29 +0300

    new update

--- js/script.js
+++ js/script.js
@@ -1,33 +1,49 @@
 // https://script.google.com/macros/s/AKfycbxdeDGV_j95ohIIDnSSpXI5iZeAaRduUSrEscUQhbqD0S7bhyB1Lv02NLROPG_fWNQ9qw/exec
 // Function to handle the form submission
-function handleFormSubmit(event) {
-  event.preventDefault(); // Prevent the default form submission behavior
-
-  // Submit the form data using AJAX
-  var form = event.target;
-  var url = form.action;
-  var formData = new FormData(form);
-
-  // AJAX request
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", url, true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      // Manually redirect to the thank-you page
-      window.location.href = "thankyou.html";
-    }
-  };
-  xhr.send(formData);
-}
 
 document.addEventListener("DOMContentLoaded", function () {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.ipify.org?format=json", true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var data = JSON.parse(xhr.responseText);
+  var form = document.getElementById("myForm");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Get form data
+    var formData = new FormData(form);
+
+    // Send form data to the server
+    fetch(
+      "https://script.google.com/macros/s/AKfycbx1r4zUC-gXaiRp4HUZXb08JfZ_VS1zRWPYHV4ns9OvboRI4xYcqu1T03V29HxYa1TUbQ/exec",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then(function (response) {
+        if (response.ok) {
+          window.location.href = "thankyou.html"; // Redirect to thankyou.html upon successful form submission
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        // Handle error case
+      });
+  });
+
+  // Retrieve IP address
+  fetch("https://api.ipify.org?format=json")
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Error: " + response.status);
+      }
+    })
+    .then(function (data) {
       document.getElementById("ip-input").value = data.ip;
-    }
-  };
-  xhr.send();
+    })
+    .catch(function (error) {
+      console.log(error);
+      // Handle error case
+    });
 });
