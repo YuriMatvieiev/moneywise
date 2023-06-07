2d47d66a498b2123fc5c8e560c4feb1567b9d2f6
Yuri Matvieiev <matvssv.yura@gmail.com>
2023-06-07 14:21:48 +0300

    added local storage for form state

--- js/script.js
+++ js/script.js
@@ -1,27 +1,48 @@
 // https://script.google.com/macros/s/AKfycbxdeDGV_j95ohIIDnSSpXI5iZeAaRduUSrEscUQhbqD0S7bhyB1Lv02NLROPG_fWNQ9qw/exec
 // Function to handle the form submission
 document.addEventListener("DOMContentLoaded", function () {
-  var form = document.querySelector(".article__form");
-  var mainForm = document.querySelector(".article__main-form");
-  var thankyou = document.querySelector(".article__thankyou");
+  var forms = document.querySelectorAll(".article__form");
+  var mainForms = document.querySelectorAll(".article__main-form");
+  var thankyous = document.querySelectorAll(".article__thankyou");
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
+  var formSubmitted = localStorage.getItem("formSubmitted");
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", form.action, true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        // Form submission successful
-        mainForm.style.display = "none"; // Hide the main form
-        thankyou.style.display = "block"; // Show the thank you message
-      }
-    };
+  if (formSubmitted) {
+    mainForms.forEach(function (mainForm) {
+      mainForm.style.display = "none"; // Hide the main form
+    });
 
-    // Get form data
-    var formData = new FormData(form);
-    xhr.send(formData);
-  });
+    thankyous.forEach(function (thankyou) {
+      thankyou.style.display = "block"; // Show the thank you message
+    });
+  } else {
+    forms.forEach(function (form) {
+      form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", form.action, true);
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            // Form submission successful
+            mainForms.forEach(function (mainForm) {
+              mainForm.style.display = "none"; // Hide the main form
+            });
+
+            thankyous.forEach(function (thankyou) {
+              thankyou.style.display = "block"; // Show the thank you message
+            });
+
+            localStorage.setItem("formSubmitted", true); // Set flag indicating form submission
+          }
+        };
+
+        // Get form data
+        var formData = new FormData(form);
+        xhr.send(formData);
+      });
+    });
+  }
 
   // Fetch IP address
   var xhr = new XMLHttpRequest();
@@ -29,7 +50,10 @@ document.addEventListener("DOMContentLoaded", function () {
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
       var data = JSON.parse(xhr.responseText);
-      document.getElementById("ip-input").value = data.ip;
+      var ipInputs = document.querySelectorAll("#ip-input");
+      ipInputs.forEach(function (ipInput) {
+        ipInput.value = data.ip;
+      });
     }
   };
   xhr.send();
